208f5a75e9ee63bb1c940bbf0b43a7d053f39e02
shinjons99 <shinjons99@gmail.com>
2023-03-09 10:30:45 +0600

    first commit

---
+++ try-catch.js
@@ -0,0 +1,29 @@
+function checkAge(){
+    const ageField = document.getElementById('age');
+    const ageText = ageField.value;
+    // ageField.value = '';
+    const errorTag = document.getElementById('error');
+    
+    try{
+      const age = parseInt(ageText)
+      if(isNaN(age)){
+        // console.log('age not found', age, ageText);
+        throw "Please enter a number"
+      }
+      else if(age < 18){
+        throw "Baccha kaccha not allowed";
+      }
+      else if (age > 30){
+        throw "Murubbira not allowed";
+      }
+    errorTag.innerHTML = '';
+    }
+    catch(err){
+        console.log('ERROR:', err);
+        errorTag.innerHTML = 'ERROR:' + err;
+    }
+    finally{
+        console.log('All done inside try catch');
+    }
+    console.log(111111);
+}
